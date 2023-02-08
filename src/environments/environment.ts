ab1dc87cd206af4f5fa49cbd19c0a03685f15327
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-02-08 19:51:08 +0100

    update IPs of the Azure services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  clientsUrl: 'http://20.241.255.156/api/v1/clients',
-  booksUrl: 'http://20.253.121.212/api/v1/books',
-  cartsUrl: 'http://20.253.122.38/api/v1/carts',
-  storageUrl: 'http://20.253.121.108/api/v1/storage',
-  ordersUrl: 'http://20.253.122.152/api/v1/orders',
-  ratingsUrl: 'http://20.121.108.205/api/v1/ratings',
+  clientsUrl: 'http://20.120.65.209/api/v1/clients',
+  booksUrl: 'http://20.42.35.150/api/v1/books',
+  cartsUrl: 'http://20.42.36.204/api/v1/carts',
+  storageUrl: 'http://20.42.39.232/api/v1/storage',
+  ordersUrl: 'http://52.188.42.70/api/v1/orders',
+  ratingsUrl: 'http://20.185.8.197/api/v1/ratings',
   verGUI: '1.0.0',
   dateGUI: 'Feb-05-2023'
 };
