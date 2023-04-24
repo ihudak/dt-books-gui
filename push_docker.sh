622769cd0d69ed71e8717d9b84e20fbc7746ee7e
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-24 08:20:16 +0200

    make part of the services instrumented with Otel and part with OA

--- push_docker.sh
+++ push_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ng build
-docker image build --platform linux/amd64 -t ihudak/dt-bookstore-webapp:latest .
-docker push ihudak/dt-bookstore-webapp:latest
+docker image build --platform linux/amd64 -t ivangudak096/dt-bookstore-webapp:latest .
+docker push ivangudak096/dt-bookstore-webapp:latest
 
-docker image build --platform linux/arm64 -t ihudak/dt-bookstore-webapp:arm64 .
-docker push ihudak/dt-bookstore-webapp:arm64
+#docker image build --platform linux/arm64 -t ivangudak096/dt-bookstore-webapp:arm64 .
+#docker push ivangudak096/dt-bookstore-webapp:arm64
