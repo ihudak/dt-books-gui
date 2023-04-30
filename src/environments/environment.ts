549a9d15cf9c57cc3f2b05af8d11cdd0121deba6
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-04-30 13:43:44 +0200

    fix IPs on the new Azuke K8S deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  clientsUrl: 'http://52.226.211.189/api/v1/clients',
-  booksUrl: 'http://52.226.212.53/api/v1/books',
-  cartsUrl: 'http://52.226.214.22/api/v1/carts',
-  storageUrl: 'http://52.226.214.90/api/v1/storage',
-  ordersUrl: 'http://52.226.214.166/api/v1/orders',
-  ratingsUrl: 'http://52.226.214.221/api/v1/ratings',
+  clientsUrl: 'http://20.120.126.92/api/v1/clients',
+  booksUrl: 'http://20.120.127.94/api/v1/books',
+  cartsUrl: 'http://20.120.127.168/api/v1/carts',
+  storageUrl: 'http://20.120.124.67/api/v1/storage',
+  ordersUrl: 'http://20.121.224.83/api/v1/orders',
+  ratingsUrl: 'http://20.121.224.233/api/v1/ratings',
   verGUI: '1.0.1',
   dateGUI: 'Apr-29-2023'
 };
